83399b188aa4c246e28604bac84a1f53bd2ef5a7
Levi Tan Ong <levi@byimplication.com>
2014-12-18 17:30:25 +0800

    sass

--- app.js
+++ app.js
@@ -54,17 +54,22 @@ var App = React.createClass({
           <div className="flex"></div>
         </div>
         <div className="fit layout vertical center">
-          <div className="butthurt-container">
-            <h1>{daysSince} days since last butthurt</h1>
-            <p>
-              Butthurt is a tribute to Filipino (over)sensitivities, and nothing more.
-            </p>
-            <p>
-              If you'd like to suggest a new entry, click <a className="inline" href="https://docs.google.com/forms/d/1Cexh4k5-nfwX9RKmv7Dwd4DcGweXLh8Kn6iZXbob0hU/viewform">here</a> or drop us a <a className="inline" href="https://github.com/torchapps/butthurt">pull request</a>!
-            </p>
-            <p>
-              Made by <a className="inline" href="https://torchapps.github.io/">Torch</a>
-            </p>
+          <div className="butthurt-container horizontal layout">
+            <div className="side">
+              <h1>{daysSince}</h1>
+            </div>
+            <div className="main flex">
+              <h1>days since last butthurt</h1>
+              <p>
+                Butthurt is a tribute to Filipino (over)sensitivities, and nothing more.
+              </p>
+              <p>
+                If you'd like to suggest a new entry, click <a className="inline" href="https://docs.google.com/forms/d/1Cexh4k5-nfwX9RKmv7Dwd4DcGweXLh8Kn6iZXbob0hU/viewform">here</a> or drop us a <a className="inline" href="https://github.com/torchapps/butthurt">pull request</a>!
+              </p>
+              <p>
+                Made by <a className="inline" href="https://torchapps.github.io/">Torch</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
