7e4f100a0e7935cafee105a8386f2f3f32615c05
Levi Tan Ong <levi@byimplication.com>
2014-12-18 14:42:39 +0800

    modifies layout.css to be compatible with react

--- app.js
+++ app.js
@@ -49,7 +49,24 @@ var App = React.createClass({
     var daysSince = this.daysSinceDate(this.latestDate(this.state.butthurts));
     return (
       <div>
-        {daysSince}
+        <div id="background" className="horizontal layout">
+          <div className="sidebar"></div>
+          <div className="flex"></div>
+        </div>
+        <div className="fit layout vertical center">
+          <div className="butthurt-container">
+            <h1>{daysSince} days since last butthurt</h1>
+            <p>
+              Butthurt is a tribute to Filipino (over)sensitivities, and nothing more.
+            </p>
+            <p>
+              If you'd like to suggest a new entry, click <a className="inline" href="https://docs.google.com/forms/d/1Cexh4k5-nfwX9RKmv7Dwd4DcGweXLh8Kn6iZXbob0hU/viewform">here</a> or drop us a <a className="inline" href="https://github.com/torchapps/butthurt">pull request</a>!
+            </p>
+            <p>
+              Made by <a className="inline" href="https://torchapps.github.io/">Torch</a>
+            </p>
+          </div>
+        </div>
       </div>
     )
   }
