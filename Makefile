5290f8dadce8e02639f65628141dcb41705d03f0
Levi Tan Ong <levi@byimplication.com>
2015-02-05 16:04:53 +0800

    makefile and webpack server

---
+++ Makefile
@@ -0,0 +1,20 @@
+all: development
+
+clean:
+	@test -d build && rm -r build || true
+
+deps:
+	@npm install
+
+development: clean deps
+	./node_modules/.bin/webpack-dev-server --inline --hot --content-base .
+
+github: development
+	git checkout gh-pages
+	rm -rf img/
+	mv build/* .
+	rmdir build
+	git add .
+	git commit -m "updated"
+	git push origin gh-pages
+	git checkout react
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,16 @@ Config = {
         // Query parameters are passed to node-sass
         loader: "style!css!sass?outputStyle=expanded&" +
           "includePaths[]=" + bourbon
-      }
+      },
+      // for fonts
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff2" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" },
+      { test: /\.png(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/png" },
+      { test: /\.jpg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/jpeg" },
+      { test: /\.ico(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/x-icon" }
     ]
   }
 };
